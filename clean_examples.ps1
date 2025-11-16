bc3eeb964a55f4ec8080fcb87fd536e2e5ec1a1f
Ian Rash <redcodefinal@gmail.com>
2025-11-15 18:37:03 -0800

    Add file to clean out the example stuff

---
+++ clean_examples.ps1
@@ -0,0 +1,5 @@
+# Deletes the contents of the /maps directory
+Remove-Item -Path "maps\*" -Recurse -Force
+
+# Replaces the contents of main.gd with "extends Node3D"
+Set-Content -Path "main.gd" -Value "extends Node3D"
