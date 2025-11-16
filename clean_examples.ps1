62bef54fc42a838a33f3e8223bfa2b134d4fd27b
Ian Rash <redcodefinal@gmail.com>
2025-11-15 20:45:13 -0800

    Add cleaner

--- clean_examples.ps1
+++ clean_examples.ps1
@@ -1,5 +1,6 @@
 # Deletes the contents of the /maps directory
 Remove-Item -Path "maps\*" -Recurse -Force
+Remove-Item -Path "dialogue\*" -Recurse -Force
 
 # Replaces the contents of main.gd with "extends Node3D"
 Set-Content -Path "main.gd" -Value "extends Node3D"
